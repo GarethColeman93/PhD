92a8ff0706a9bd3ff7c7347c7975371e56aae52b
GarethColeman93 <35695275+GarethColeman93@users.noreply.github.com>
2019-07-08 12:31:24 +0100

    Add files via upload
    Script that takes BLAST output (format 6) and pulls out sequences from database and writes to file. Modified by Ed Moody.

---
+++ match_ids_to_seqs_final.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 15 14:04:14 2017
+
+@author: gc12847
+"""
+### Script to take BLAST output (format 6) and match the database sequences and write to file ###
+
+from Bio import SeqIO
+import sys   
+
+#this is the database file .tsv
+filename = sys.argv[1]
+
+#this a concatenated fasta file of all the sequences used to make the original blastdb
+allsequences = sys.argv[2]
+
+#this is output file
+outfile = sys.argv[3] 
+
+f = open(filename, 'r')
+seqs = {}
+for seq_record in SeqIO.parse(allsequences, 'fasta'):
+	seqs[seq_record.id] = seq_record
+i = 0
+idList = {}
+for line in f:
+	lst = line.split()
+	idList = lst
+	writefile = open(outfile, 'a') #must use a to append, or will overwrite each time!
+	for j in idList:
+		if j in seqs:
+			SeqIO.write(seqs[j], writefile, "fasta")
+	i+=1
