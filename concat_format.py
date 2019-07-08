72eee6704a2c82ff87626c98724e6e983a0cc2b1
GarethColeman93 <35695275+GarethColeman93@users.noreply.github.com>
2019-07-08 13:14:35 +0100

    To reformat raw NCBI names to those suitable for concatenation script

---
+++ concat_format.py
@@ -0,0 +1,21 @@
+#Script to reformat names in fasta alignment files for concatentation
+
+import fileinput
+import glob
+
+file_paths = sys.argv[1]
+files = glob.glob(file_paths)
+for file in files:
+	with fileinput.FileInput(file, inplace=True, backup='.backup') as file:
+		for line in file:
+			if line.startswith('>'):
+				if ']' in line:
+					line = line.replace(']', '')
+					lst = line.split('[')
+					if '[00-' in line:
+						print('>', lst[1], end='', sep='')
+					else:
+						print('>', lst[2], end='', sep='')
+			else:
+				print(line, sep='', end='')
+		
