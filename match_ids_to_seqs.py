a5116dce435014352cc98d2b191e5653d700af60
GarethColeman93 <35695275+GarethColeman93@users.noreply.github.com>
2019-07-08 13:33:11 +0100

    Match output of MCL to original BLAST database sequences and write to separate files

---
+++ match_ids_to_seqs.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 15 14:04:14 2017
+
+@author: gc12847
+"""
+    
+mcl_file = sys.argv[1]
+seqfile = sys.argv[2]
+
+f = open ('/Users/garethcoleman/Documents/uni work/PhD work/Bacteria_rooting/gene_trees/out.seq.mci.I15', 'r')
+from Bio import SeqIO
+#seqs = SeqIO.index('ALLseqs.fas', 'fasta') #seq is a dictionary, key = record IDs, values = records
+seqs = {}
+for seq_record in SeqIO.parse(seqfile,'fasta'):
+    seqs[seq_record.id] = seq_record    
+i = 0
+idList = {}
+for line in f:
+    lst = line.split()
+    idList = lst
+    writefile = open("Output_%i" %i, 'w')
+    for a in idList:
+        if a in seqs:
+            SeqIO.write(seqs[a],writefile, "fasta")
+        #for seq_record in SeqIO.parse('ALLseqs.fas', 'fasta'):
+         #   if seq_record.id == a:
+                #print (seq_record.id)
+          #      SeqIO.write(seq_record,writefile, "fasta")
+                #print(len(seq_record))
+    i+=1
